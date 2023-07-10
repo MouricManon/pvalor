9631b6d7b8fd0520757bd1eb11d429b04d1248e9
Manonmouric <manon.mouric@gmail.com>
2023-07-10 10:43:56 -0400

    Body.tsx fonctionnel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Requests from './Requests'
+import Profile from './Profile'
 
 function App() {
   return (
     <div className="App">
-
+<Profile></Profile>
       <Requests></Requests>
     </div>
   );
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,53 +1,21 @@
 import { useEffect, useState } from 'react'
-type BodyProps = {
-  date : string
-}
 
 
-function Body({ date }: BodyProps) {
-    const [donnees, setDonnees] = useState<any>(null); 
-    const [fat, setFat] = useState<any>(null); 
-    const [weight, setWeight] = useState<any>(null); 
-    const [temp, setTemp] = useState<any>(null); 
-    const[avgfat,setAvgfat]=useState(1)
-    const[avgweight,setAvgweight]=useState(1)
-    const[avgtemp,setAvgtemp]=useState(1)
+
+function Profile() {
     const[token,setToken]=useState("eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1I3NzYiLCJzdWIiOiJCTUJDUVYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJwcm8gcm51dCByc2xlIHJjZiByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjg5MDI0ODc2LCJpYXQiOjE2ODg5OTYwNzZ9.qd0-x39pdxlb60f7HUuJk5uo2gL_RchtTgJFKkyO4hY")
     const[id,setid]=useState("-")
-    useEffect(()=>{ getFat().then(r=>setFat(r)); getWeight().then(r=>setWeight(r));
-    getTemp().then(r=>setTemp(r))},[date]
+
+    useEffect(()=>{ }
 )
 
-async function getFat(){
+async function getProfile(){
 
-// Utilisation de l'API Fetch pour effectuer une requête HTTP GET
-let url = "https://api.fitbit.com/1/user/" + id + "/body/log/fat/date/" + date + ".json ";
+//get profile
 
-fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + token,
-        },
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Les données récupérées sont stockées dans la variable 'data'
-        console.log(data);
-        //function async je lui fais un return data je le mets dans un useEffect{(()=>{ fctionasync().then(r=>setMass(r))},[date])}} 
+let urlprofile = "https://api.fitbit.com/1/user/" + id + "/profile.json ";
 
-        // Traitez les données ici
-    })
-    .catch(error => {
-        // Gestion des erreurs
-        console.error('Une erreur s\'est produite:', error);
-    });
-}
-async function getWeight(){
-    
-// Get body weight
-
-let urlbodyweight = "https://api.fitbit.com/1/user/" + id + "/body/log/weight/date/" + date + ".json ";
-fetch(urlbodyweight, {
+fetch(urlprofile, {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + token,
@@ -63,31 +31,6 @@ fetch(urlbodyweight, {
         // Gestion des erreurs
         console.error('Une erreur s\'est produite:', error);
     });
-
-}
-async function getTemp(){
-    
-//get temperature skin
-
-let urltemp = "https://api.fitbit.com/1/user/" + id + "/temp/skin/date/" + date + ".json ";
-
-fetch(urltemp, {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + token,
-        },
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Les données récupérées sont stockées dans la variable 'data'
-        console.log(data);
-        // Traitez les données ici
-    })
-    .catch(error => {
-        // Gestion des erreurs
-        console.error('Une erreur s\'est produite:', error);
-    });
-
 }
 function fatdisplay(){
       let somme = 0;
@@ -148,4 +91,4 @@ function isnulltemp(){
     <div>{isnullweight()}</div>
     <div>{isnulltemp()}</div></div>)
 }
-export default Body;
+export default Profile;
