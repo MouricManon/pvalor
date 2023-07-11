b572cb8a960dbe7b75f85c89d86f167f5ba1300e
Manonmouric <manon.mouric@gmail.com>
2023-07-11 10:14:37 -0400

    Update Devices.tsx

--- src/Devices.tsx
+++ src/Devices.tsx
@@ -7,10 +7,7 @@ type DevicesProps = {
   
 
 function Devices({token, id }: DevicesProps) {
-const[modele,setModele]=useState([])
-const[lastsync,setLastsync]=useState([])
 const[html, setHtml]=useState("")
-const[type,setType]=useState([])
 useEffect(()=>{ getDevices()}
 )
 
@@ -28,19 +25,22 @@ fetch(urldevice, {
     .then(response => response.json())
     .then(data => {
         // Les données récupérées sont stockées dans la variable 'data'
-        for(let device of data){
-       setModele(modele.concat(device.deviceVersion));
-        setLastsync(lastsync.concat(device.lastSyncTime));
-        setType(type.concat(device.type))}
-        console.log(data)
-        // Traitez les données ici
+     let modele=[]
+    let lastsync=[]
+    let type=[]
+       for(let i=0;i<data.length;i++){
+        modele.add(data[i].deviceVersion)
+        lastsync.add(data[i].lastSyncTime)
+        type.add(data[i].type)
+       }
+      displaydevice(modele, lastsync, type)
     })
     .catch(error => {
         // Gestion des erreurs
         console.error('Une erreur s\'est produite:', error);
     });
 }
-function displaydevice(){
+function displaydevice(modele: string | any[] | undefined, lastsync: any[] | undefined, type: any[] | undefined){
     let htmlval = ""
   for(let i=0;i<modele.length;i++){
      htmlval+=<li>Modele : {modele[i]} , Last sync :{lastsync[i]}, Type : {type[i]}</li>
